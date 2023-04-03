0c6216803b2356932b372c7d337e4fe3df6426d8
miten09 <mitensoni108@gmail.com>
2023-04-03 15:50:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import "./App.css";
 function App() {
   const [movies, setmovies] = useState([]);
   const [isLoading, setisLoading] = useState(false);
+  const [error,seterror]=useState(null)
 
   async function fetchMoviesHandler() {
     setisLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    setmovies(data.results);
-    setisLoading(false);
+    seterror(null)
+
+    
+    try{
+      const response = await fetch("https://swapi.dev/api/films");
+      if(!response.ok){
+        throw new Error('something went wrong !!')
+      }
+      const data = await response.json();
+
+   
+      setmovies(data.results);
+    
+    }
+   catch(error){
+     seterror(error.message)
+   }
+   setisLoading(false);
   }
 
   return (
@@ -23,8 +38,9 @@ function App() {
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
-        {!isLoading && movies.length ===0 && <p>Found No movies.</p>}
+        {!isLoading && movies.length ===0 && !error && <p>Found No movies.</p>}
         {isLoading && <p>Loading...</p>}  
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
